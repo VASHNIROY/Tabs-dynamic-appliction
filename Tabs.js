ca779bde7e9c6c13e97d07873169a3933e9b50c7
VASHNIROY <vashniroynallapu@gmail.com>
2023-05-03 12:53:40 +0530

    first commit

---
+++ Tabs.js
@@ -0,0 +1,40 @@
+let aboutButton = document.getElementById("aboutButton");
+let timeToVisitButton = document.getElementById("timeToVisitButton");
+let attractionsButton = document.getElementById("attractionsButton");
+
+let aboutTab = document.getElementById("aboutTab");
+let timeToVisitTab = document.getElementById("timeToVisitTab");
+let attractionsTab = document.getElementById("attractionsTab");
+
+timeToVisitTab.classList.add("d-none");
+attractionsTab.classList.add("d-none");
+
+function about() {
+    aboutTab.classList.remove("d-none");
+    timeToVisitTab.classList.add("d-none");
+    attractionsTab.classList.add("d-none");
+
+    aboutButton.classList.add("selected-button");
+    timeToVisitButton.classList.remove("selected-button");
+    attractionsButton.classList.remove("selected-button");
+}
+
+function visit() {
+    aboutTab.classList.add("d-none");
+    timeToVisitTab.classList.remove("d-none");
+    attractionsTab.classList.add("d-none");
+
+    aboutButton.classList.remove("selected-button");
+    timeToVisitButton.classList.add("selected-button");
+    attractionsButton.classList.remove("selected-button");
+}
+
+function attractions() {
+    aboutTab.classList.add("d-none");
+    timeToVisitTab.classList.add("d-none");
+    attractionsTab.classList.remove("d-none");
+
+    aboutButton.classList.remove("selected-button");
+    timeToVisitButton.classList.remove("selected-button");
+    attractionsButton.classList.add("selected-button");
+}
